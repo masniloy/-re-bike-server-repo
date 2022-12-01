d057084230e4e599cfad35570194eeec85146fa6
masniloy <masaimunniloy88@gmail.com>
2022-12-01 20:37:13 +0600

    grt data from mongo

--- index.js
+++ index.js
@@ -7,6 +7,32 @@ app.use(express.json());
 require('dotenv').config();
 
 
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.g924zd8.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const categoryCollections = client.db('re-bike').collection('category')
+        app.get('/category', async (req, res) => {
+            const query = {}
+            const categoryOptions = await categoryCollections.find(query).toArray();
+            res.send(categoryOptions);
+        })
+        app.get('/category/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const oneCategoryOptions = await categoryCollections.findOne(query);
+            res.send(oneCategoryOptions);
+        })
+
+    }
+    finally {
+
+    }
+}
+run().catch(err => console.log(err));
+
 
 app.get('/', (req, res) => {
     res.send('ok');
